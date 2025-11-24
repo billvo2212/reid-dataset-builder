5a9ede6ae1e5478fe89c5bf67b4d5ce5faef36b4
billvo <kvvo@student.ubc.ca>
2025-11-24 14:39:21 -0800

    Update app_production.py

--- app_production.py
+++ app_production.py
@@ -382,7 +382,7 @@ def download(session_id):
 
 if __name__ == '__main__':
     # Local development
-    app.run(debug=True, host='0.0.0.0', port=5005)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 else:
     # Production (for Gunicorn)
     pass
